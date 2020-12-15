f4c8624eb9115b93b988318dfb73d8b85d4ce730
Emanuil Tolev <emanuil.tolev@gmail.com>
2020-12-15 10:06:23 +0000

    move Ruby logging config to (much) better location

--- rbapp/supermarketwarehouse/config/application.rb
+++ rbapp/supermarketwarehouse/config/application.rb
@@ -15,5 +15,24 @@ module Supermarketwarehouse
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.logger = ActiveSupport::Logger.new(File.join(Rails.root, 'log', "supermarketwarehouse.log"))
+    config.logger.datetime_format = "%Y-%m-%d %H:%M:%S"
+
+    class MyAppFormatter < Logger::Formatter
+      def call(severity, datetime, progname, msg)
+        ElasticAPM.log_ids do |transaction_id, span_id, trace_id|
+          {
+            timestamp: "#{datetime}\n",
+            severity: "#{severity}\n",
+            message: "#{msg}",
+            :'trace.id' => trace_id,
+            :'transaction.id' => transaction_id,
+            :'span.id' => span_id
+          }.to_json + "\r\n"
+        end
+      end
+    end
+    config.logger.formatter = MyAppFormatter.new
   end
 end
--- rbapp/supermarketwarehouse/config/environments/development.rb
+++ rbapp/supermarketwarehouse/config/environments/development.rb
@@ -59,22 +59,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.logger = ActiveSupport::Logger.new(File.join(Rails.root, 'log', "supermarketwarehouse.log"))
-  config.logger.datetime_format = "%Y-%m-%d %H:%M:%S"
-
-  class MyAppFormatter < Logger::Formatter
-    def call(severity, datetime, progname, msg)
-      ElasticAPM.log_ids do |transaction_id, span_id, trace_id|
-        {
-          timestamp: "#{datetime}\n",
-          severity: "#{severity}\n",
-          message: "#{msg}",
-          :'trace.id' => trace_id,
-          :'transaction.id' => transaction_id,
-          :'span.id' => span_id
-        }.to_json + "\r\n"
-      end
-    end
-  end
-  config.logger.formatter = MyAppFormatter.new
 end
